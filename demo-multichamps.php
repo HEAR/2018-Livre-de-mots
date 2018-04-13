fb45af93d628ae2fad4c930d6c6b47ee28561965
Loïc Horellou <hello@loichorellou.net>
2018-04-13 09:19:30 +0200

    ajout demo-multichamps.php

---
+++ demo-multichamps.php
@@ -0,0 +1,39 @@
+<?php
+
+// ici c'est le bloc pour préparer la librairie permettant l'analyse du fichier CSV
+
+require_once("parsecsv-for-php/parsecsv.lib.php");
+
+// on déclare le parser (l'analyseur)
+$csv = new ParseCsv\Csv();
+$csv->encoding('UTF-16', 'UTF-8');
+$csv->delimiter = ";";
+
+// on parse les données (on les analyse, on les filtre)
+$csv->parse('tableau-de-mots.csv');
+
+
+// print_r($csv->data);
+
+
+$xml = "<livre>";
+
+foreach( $csv->data as $ligne ){
+
+	$xml .= "<nom><prenom>".$ligne["Prénom"] . "</prenom> " . $ligne["Nom"]."</nom>\n";
+	
+	$xml .= "<mot>".$ligne["Mot"] . "</mot>\n";
+	
+	$xml .= "<definition>".$ligne["Définition"] . "</definition>\n";
+
+}
+
+$xml .= "</livre>";
+
+
+$xml = str_replace("&", "&amp;", $xml);
+
+// echo $xml;
+
+file_put_contents("livre-multichamps.xml", $xml);
+
